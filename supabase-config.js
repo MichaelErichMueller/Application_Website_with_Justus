526a8ce28774b79c7379b772092fface9d5b7601
MichaelErichMueller <Michael.Erich.Mueller@gmx.de>
2026-05-18 22:51:27 +0200

    Connect application tracker to Supabase

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "",
-  anonKey: ""
+  url: "https://iierfmgygtforhfbfywb.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlpZXJmbWd5Z3Rmb3JoZmJmeXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxMjI5MjEsImV4cCI6MjA5NDY5ODkyMX0.e6IhI4Jmg4H9cdIN6O5VHojiuTWv8OEZKn0SwYNFdYc"
 };
